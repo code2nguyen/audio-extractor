571eeca969a0d806ecea3b2f36c130893141d0a0
Nguyen Thai Vinh <thaivinh.nguyen@finastra.com>
2021-11-19 02:21:41 +0100

    fix timeout

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg');
-const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+const timeout = require('connect-timeout');
 
-const ffmpeg = createFFmpeg({ log: true });
-const PUBLIC_FOLDER = 'media'
+const ffmpeg = createFFmpeg({ log: false });
+const PUBLIC_FOLDER = 'media';
 
-app.get('/extract', async (req, res) => {
-    const videoUrl = req.query.videoUrl;
-    const fileName = req.query.fileName;
+app.get('/extract', timeout('25s'), haltOnTimedout, async (req, res) => {
+  const videoUrl = req.query.videoUrl;
+  const fileName = req.query.fileName;
+  const outputFileName = req.query.outputFileName;
 
-    if (videoUrl) {
-        if (!ffmpeg.isLoaded()) {
-            await ffmpeg.load();
-        }
-        ffmpeg.FS('writeFile', fileName, await fetchFile(videoUrl));
-        const audioFileName = `${uuidv4()}.mp3`;
-        await ffmpeg.run('-i', fileName, audioFileName);
-        const output = ffmpeg.FS('readFile', audioFileName);
-        await ffmpeg.FS('unlink', audioFileName);
-        await fs.promises.writeFile(`${PUBLIC_FOLDER}/${audioFileName}`, output);
-        return res.status(200).send(audioFileName)
+  if (videoUrl) {
+    if (!ffmpeg.isLoaded()) {
+      await ffmpeg.load();
     }
-    
-    res.status(501).send('Not found videoUrl')
-})
+    ffmpeg.FS('writeFile', fileName, await fetchFile(videoUrl));
+    const audioFileName = outputFileName;
+    await ffmpeg.run('-i', fileName, audioFileName);
+    const output = ffmpeg.FS('readFile', audioFileName);
+    await ffmpeg.FS('unlink', audioFileName);
+    await fs.promises.writeFile(`${PUBLIC_FOLDER}/${audioFileName}`, output);
 
-app.use(express.static(PUBLIC_FOLDER))
+    if (req.timedout) return
+    return res.status(200).send(audioFileName);
+  }
+
+  res.status(501).send('Not found videoUrl');
+});
+
+app.use(express.static(PUBLIC_FOLDER));
 
 app.listen(process.env.PORT || 8080, '0.0.0.0');
+
+function haltOnTimedout(req, res, next) {
+  if (!req.timedout) next();
+}
